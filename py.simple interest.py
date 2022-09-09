66fb37150079e77ed1ec7981cc17d05d7ecdcf16
Jayaprakashvarma <112853428+Jayaprakashvarma@users.noreply.github.com>
2022-09-09 09:53:47 +0530

    Update py.simple interest.py

--- py.simple interest.py
+++ py.simple interest.py
@@ -1,6 +1,6 @@
-P=5000 #principal amount
-R=15 #rate
-T=1 #time
-SI=(P*R*T)/100;#simple interest calculation
+P=5000 
+R=15 
+T=1 
+SI=(P*R*T)/100;
 print("simple interest is:");
-print(SI); #prints simple interest
+print(SI); 
