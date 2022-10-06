e489b68f64d37b9382c46f6e061438c696291969
Jayaprakashvarma <112853428+Jayaprakashvarma@users.noreply.github.com>
2022-10-06 14:36:13 +0530

    Add files via upload

---
+++ perfect squares day1.10.py
@@ -0,0 +1,9 @@
+def perfectsquares(l ,r):
+    for i in range(l,r+1):
+        if (i**(.5)== int(i**(.5))):
+            print(i, end=" ")
+            l=int(input("enter the starting range="))
+            r=int(input("enter the ending range="))
+perfectsquares(1,r)
+  
+ 
