cd26d5542d92934d3c1416fd0f2f7463a988016a
Jayaprakashvarma <112853428+Jayaprakashvarma@users.noreply.github.com>
2022-10-10 14:51:20 +0530

    Add files via upload

---
+++ decimal to binary .py
@@ -0,0 +1,8 @@
+num = list(input("Input a binary number: "))
+value = 0
+
+for i in range(len(num)):
+	digit = num.pop()
+	if digit == '1':
+		value = value + pow(2, i)
+print("The decimal value of the number is", value)
